f93af49e6e6b0a435c07c240d24d1623106ff35d
Nathan Bean <nhbean@WIN2.CS.KSU.EDU>
2019-11-14 15:36:51 -0600

    in-class work

--- TicTacToe/wwwroot/js/site.js
+++ TicTacToe/wwwroot/js/site.js
@@ -2,3 +2,34 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+var selectedChecker;
+
+
+
+function onClickChecker(event) {
+    if (selectedChecker) {
+        selectedChecker.setAttribute("stroke", "gray");
+    }
+    selectedChecker = event.target;
+    event.target.setAttribute("stroke", "green");
+}
+
+function onClickSquare(event) {
+    if (selectedChecker) {
+        document.getElementById("cx").value = selectedChecker.getAttribute("data-x");
+        document.getElementById("cy").value = selectedChecker.getAttribute("data-y");
+        document.getElementById("sx").value = event.target.getAttribute("data-x");
+        document.getElementById("sy").value = event.target.getAttribute("data-y");
+        document.getElementById("move-form").submit();
+    }
+}
+
+var checkers = document.getElementsByTagName("circle");
+for (var i = 0; i < checkers.length; i++) {
+    checkers[i].addEventListener('click', onClickChecker);
+}
+
+var squares = document.getElementsByTagName("rect");
+for (var j = 0; j < squares.length; j++) {
+    squares[j].addEventListener('click', onClickSquare);
+}
