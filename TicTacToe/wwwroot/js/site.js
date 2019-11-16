31823585c953be3560f2ce73769a440848f5fdea
engg-autologin <engg-autologin@users.campus>
2019-11-15 18:03:22 -0600

    added in-class work

--- TicTacToe/wwwroot/js/site.js
+++ TicTacToe/wwwroot/js/site.js
@@ -2,3 +2,34 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var squares = document.querySelectorAll("#checkers-board > .square");
+for (var i = 0; i < squares.length; i++)
+{
+    squares[i].addEventListener('dragenter', onDragEnter);
+    squares[i].addEventListener('dragleave', onDragLeave);
+    squares[i].addEventListener('dragstart', onDragStart);
+    squares[i].addEventListener('dragend', onDragEnd);
+}
+
+function onDragEnter(event) {
+    if (event.target.classList.contains("checker")) return;
+    if (event.target.classList.contains("red")) return;
+    if (event.target.children.length > 0) return;
+    event.target.style.backgroundColor = "gold";
+    document.getElementById("toX").value = event.target.dataset.x;
+    document.getElementById("toY").value = event.target.dataset.y;
+}
+
+function onDragLeave(event) {
+    event.target.style.backgroundColor = null;
+}
+
+function onDragStart(event) {
+    document.getElementById("fromX").value = event.target.dataset.x;
+    document.getElementById("fromY").value = event.target.dataset.y;
+}
+
+function onDragEnd(event) {
+    document.getElementById("checkers-form").submit();
+}
